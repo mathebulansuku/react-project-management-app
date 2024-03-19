7cfb31c9fdc3f50ee89b179e90f0c8dcd8a7a890
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-19 16:12:18 +0200

    Adding state to determine which component shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/SideBar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
