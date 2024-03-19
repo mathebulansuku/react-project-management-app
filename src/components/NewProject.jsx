bb98d376ba1ecd81f379e543c1dbe6de8f14a452
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-19 15:03:25 +0200

    adding HTML attributes

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,29 @@
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <p>
+          <label>Title</label>
+          <input></input>
+        </p>
+        <p>
+          <label>Description</label>
+          <textarea />
+          <input></input>
+        </p>
+        <p>
+          <label>Due Date</label>
+          <input></input>
+        </p>
+      </div>
+    </div>
+  );
+}
