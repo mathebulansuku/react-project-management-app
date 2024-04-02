bc872eaeaedd6dab5b198a608f261b476ac5b296
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-04-02 10:40:39 +0200

    NewTask component added

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,11 @@
 export default function Tasks() {
   return (
     <section>
-      <h2>Tasks</h2>
-      NEW Tasks
-      <p></p>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      NEW TASKS
+      <p className="text-stone-800 mb-4">
+        This project does not have any tasks yet.
+      </p>
       <ul></ul>
     </section>
   );
