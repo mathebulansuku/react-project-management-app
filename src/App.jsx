630c2e9d236382aa53f4d5c7683a7531a783740b
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-04-03 08:58:26 +0200

    Update task delete button

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ function App() {
     setProjectState((prevState) => {
       return {
         ...prevState,
-        selectedProjectId: undefined,
         tasks: prevState.tasks.filter((task) => task.id !== id),
       };
     });
